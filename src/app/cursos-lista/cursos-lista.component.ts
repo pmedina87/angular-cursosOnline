d98c26c7092e5afebe198e8a449fcb4abd6c4783
Pedro Medina <pedrohugomedina@gmail.com>
2022-11-20 19:27:51 -0300

    interfaz de componente cursos-lista

--- src/app/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cursos } from './cursos';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -7,12 +8,38 @@ import { Component } from '@angular/core';
 })
 export class CursosListaComponent {
 
-  cursos = {
-    "titulo":"Curso 1",
-    "descripcion": "Curso para docentes",
-    "precio": 5420,
-    "dirigido": "Docentes nivel primario",
-    "imagen": "assets/img/docentes.jpg"
-  }
-
+  cursos: Cursos[] = [
+    {
+      titulo: 'Curso 1',
+      descripcion: 'Curso para docentes',
+      precio: 5420,
+      dirigido: "Docentes nivel primario",
+      imagen: "assets/img/docentes.jpg",
+      oferta: false,
+    },
+    {
+      titulo: 'Curso 2',
+      descripcion: 'Curso para cantantes',
+      precio: 4420,
+      dirigido: "Cantantes aficionados",
+      imagen: "assets/img/docentes.jpg",
+      oferta: true,
+    },
+    {
+      titulo: 'Curso 3',
+      descripcion: 'Curso para publico en general',
+      precio: 6420,
+      dirigido: "Publico en general",
+      imagen: "assets/img/docentes.jpg",
+      oferta: false,
+    },
+    {
+      titulo: 'Curso 4',
+      descripcion: 'Curso para publico en general',
+      precio: 16420,
+      dirigido: "Publico en general",
+      imagen: "assets/img/docentes.jpg",
+      oferta: true,
+    },
+  ];
 }
---
+++ src/app/cursos-lista/cursos.ts
@@ -0,0 +1,8 @@
+export interface Cursos {
+    titulo: string;
+    descripcion: string;
+    precio: number;
+    dirigido: string;
+    imagen: string;
+    oferta: boolean;
+}
